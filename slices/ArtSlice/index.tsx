b783228b5a14a58ac6a0886eb044d2c52cce4aca
Ayomide Jimoh <ayomidejimoh247@gmail.com>
2024-01-22 18:49:07 +0100

    feat: Import Art slice from prismic

--- slices/ArtSlice/index.tsx
+++ slices/ArtSlice/index.tsx
@@ -1,6 +1,10 @@
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
+import style from "@/styles/art.module.css";
+import Link from "next/link";
+import { PrismicNextImage } from "@prismicio/next";
+
 /**
  * Props for `ArtSlice`.
  */
@@ -14,8 +18,37 @@ const ArtSlice = ({ slice }: ArtSliceProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={style.art}
     >
-      Placeholder component for art_slice (variation: {slice.variation}) Slices
+      <div className={style.wrapper}>
+      <nav className={style.nav}>
+          <button className={style.nav__home}>{slice.primary.home_text}</button>
+          <Link href="/" className={style.nav__shop}>{slice.primary.shop_text}</Link>
+        </nav>
+
+        <div className={style.container}>
+          <div className={style.art__text}>
+            <h4>DIGITAL</h4>
+            <h4>ART</h4>
+          </div>
+
+          <div className={style.image__wrapper}>
+            {slice.items.map((item, index) => (
+              <div className={style.image__container} key={index}>
+                <div className={style.images}>
+                <PrismicNextImage field={item.image} className={style.image}/>
+                </div>
+                <div className={style.images}>
+                <PrismicNextImage field={item.image_2} className={style.image}/>
+                </div>
+                <div className={style.images}>
+                <PrismicNextImage field={item.image_3} className={style.image}/>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
