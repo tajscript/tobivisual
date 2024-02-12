1bb79497bb42ed2ebaeb2366bb1b224954eb36a9
Ayomide Jimoh <ayomidejimoh247@gmail.com>
2024-02-12 11:59:48 +0100

    feat: Create account nav component

---
+++ components/accountNav.tsx
@@ -0,0 +1,11 @@
+import style from "@/styles/nav.module.css";
+
+const accountNav = () => {
+  return (
+    <nav className={style.accountNav}>
+        <h3>TOBI ADETIMEHIN - VISUAL ARTIST</h3>
+    </nav>
+  )
+}
+
+export default accountNav
