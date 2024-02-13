279397618ef2c82e9f82e262dda0176c7781abea
Ayomide Jimoh <ayomidejimoh247@gmail.com>
2024-02-13 19:22:50 +0100

    feat: Add Prismic Images to module

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.prismic.io',
+                pathname: '**',
+            }
+        ],
+      },
+}
 
 module.exports = nextConfig
