275cb2defa1956e5a046ad7dda838a75b441a3f1
Ayomide Jimoh <ayomidejimoh247@gmail.com>
2024-02-15 21:00:19 +0100

    feat: Add middleware

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname
+    const isPublicPath = path === '/login' || path === '/register' || path === '/verify'
+
+    const token = request.cookies.get('token')?.value || ''
+
+    if(isPublicPath && token) {
+        return NextResponse.redirect(new URL('/shop', request.nextUrl))
+    }
+}
+
+export const config = {
+    matcher: [
+        '/shop',
+        '/login',
+        '/register',
+        '/verify',
+    ]
+}
