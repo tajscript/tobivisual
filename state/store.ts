7292ebaecfab121e1c638627bc4feb77060d08ad
Ayomide Jimoh <ayomidejimoh247@gmail.com>
2024-02-17 23:08:16 +0100

    feat: Update store

--- state/store.ts
+++ state/store.ts
@@ -1,14 +1,26 @@
-
-
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import cartReducer from './slice/cart';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const reducer = {
+const rootReducer = combineReducers({
   cart: cartReducer,
-};
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
-  reducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
+  })
 });
 
 export default store;
+export const persistor = persistStore(store)
