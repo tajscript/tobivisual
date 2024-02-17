0c26221b8478391cc485d98258ad4ae3d855429b
Ayomide Jimoh <ayomidejimoh247@gmail.com>
2024-02-17 23:14:59 +0100

    feat: Animate about page

--- slices/AboutSlice/index.tsx
+++ slices/AboutSlice/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
@@ -5,6 +7,12 @@ import style from "@/styles/about.module.css";
 import Link from "next/link";
 import { PrismicNextImage } from "@prismicio/next";
 import { TbAsterisk } from "react-icons/tb";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useLayoutEffect, useRef, useState } from "react";
+import FolioNav from "@/components/folioNav";
+gsap.registerPlugin(ScrollTrigger)
+
 
 /**
  * Props for `AboutSlice`.
@@ -15,26 +23,72 @@ export type AboutSliceProps = SliceComponentProps<Content.AboutSliceSlice>;
  * Component for "AboutSlice" Slices.
  */
 const AboutSlice = ({ slice }: AboutSliceProps): JSX.Element => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+  const aboutRef = useRef(null);
+  const lineRef = useRef(null);
+
+  const handleNavOpen = () => {
+    setIsNavOpen(!isNavOpen)
+  }
+
+  const handleNavClose = () => {
+    setIsNavOpen(false)
+  }
+
+  useLayoutEffect(() => {
+
+    let about = gsap.context(() => {
+    gsap.to(aboutRef.current, {duration: 1.5, delay: 0.5, opacity: 1})})
+
+    gsap.to(lineRef.current, {
+      scaleX: 10,
+      duration: 5,
+      yoyo: true,
+      repeat: -1,
+      ease: "sine.inOut"
+    })
+
+    gsap.fromTo("#details", {
+      yPercent: 25
+    }, {
+      scrollTrigger: {
+          trigger: "#details", 
+          scrub: 2, 
+          end: "bottom top"
+      },
+      yPercent: 0,
+      duration: 2,
+      yoyo: true,
+    })
+
+    return () => {
+      about.revert();
+    }
+
+    }, [])
+
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className={style.about}
     >
-      <div className={style.wrapper}>
+      <div className={style.wrapper} ref={aboutRef}>
+      <FolioNav isOpen={isNavOpen} onClose={handleNavClose} />
         <nav className={style.nav}>
-          <button className={style.nav__home}>{slice.primary.home_text}</button>
-          <Link href="/" className={style.nav__shop}>{slice.primary.shop_text}</Link>
+          <button className={style.nav__text} onClick={handleNavOpen}>{slice.primary.home_text}</button>
+          <Link href="/shop" className={style.nav__text}>{slice.primary.shop_text}</Link>
         </nav>
 
         <div className={style.container}>
           <div className={style.title}>
             <h2>{slice.primary.title}</h2>
-            <div className={style.stretch__line}></div>
             <h2>{slice.primary.title_2}</h2>
           </div>
+          <div className={style.stretch__line} ref={lineRef}></div>
 
-          <div className={style.about__details}>
+          <div className={style.about__details} id="details">
             <div className={style.image__wrapper}>
             <PrismicNextImage field={slice.primary.image} className={style.image} />
             </div>
@@ -45,8 +99,8 @@ const AboutSlice = ({ slice }: AboutSliceProps): JSX.Element => {
 
           <div className={style.visual__container}>
               <div className={style.visual__text}>
-                <h3>{slice.primary.visual_text}</h3>
-                <h3 className={style.visual__text_r}>{slice.primary.artist_text}</h3>
+                <h3 id="text_l">{slice.primary.visual_text}</h3>
+                <h3 className={style.visual__text_r} id="text_r">{slice.primary.artist_text}</h3>
               </div>
 
             <div className={style.visual__description}>
@@ -57,7 +111,7 @@ const AboutSlice = ({ slice }: AboutSliceProps): JSX.Element => {
             </div>
           </div>
 
-          <div className={style.details__about}>
+          <div className={style.details__about} id="details__about">
             <div className={style.detail__text}>
               <p>{slice.primary.description}</p>
             </div>
