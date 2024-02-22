0618ec8a58889192d4d68fd3a1000bbafe8071a9
Ayomide Jimoh <ayomidejimoh247@gmail.com>
2024-02-22 02:54:40 +0100

    feat: Refactor

--- slices/Home/index.tsx
+++ slices/Home/index.tsx
@@ -45,7 +45,6 @@ const Home = ({ slice }: HomeProps): JSX.Element => {
   useLayoutEffect(() => {
 
     let hero = gsap.context(() => {
-      gsap.to(heroRef.current, {duration: 0.3, opacity: 1, ease: 'expo.in'})
 
       gsap.fromTo(imageRef.current, {x: 0, y: 0}, {duration: 1.5, delay: 1.5, x: 10, y: -10 })
 
